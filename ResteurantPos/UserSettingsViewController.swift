cdc05779f87d326047c93d6bb4e55a5f2ea5de7d
linwei365 <linwei365@gmail.com>
2016-02-25 22:23:12 -0800

    added printer controller

---
+++ ResteurantPos/UserSettingsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  UserSettingsViewController.swift
+//  ResteurantPos
+//
+//  Created by Lin Wei on 2/25/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func printersOnClick(sender: UIButton) {
+        
+        //UIPrinterPickerController
+        let printerPicker = UIPrinterPickerController (initiallySelectedPrinter: nil)
+        //UIPrinterPickerController the modal is indicated
+        
+        
+        printerPicker.presentAnimated(true, completionHandler: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
