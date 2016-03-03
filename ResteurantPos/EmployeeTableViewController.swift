30b9c819b9dc2a954baab83bd1a64c40ca9bd15c
linwei365 <linwei365@gmail.com>
2016-03-02 16:32:02 -0800

    added editEmployee

--- ResteurantPos/EmployeeTableViewController.swift
+++ ResteurantPos/EmployeeTableViewController.swift
@@ -260,14 +260,19 @@ class EmployeeTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let vc =  segue.destinationViewController as! EditEmployeeViewController
+        
+        
+        
     }
-    */
+    
 
 }
